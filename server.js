635750ab7ac48dc07f916950869603d5ece52495
Vital-pro <vitupikov@yandex.ru>
2024-06-27 22:20:05 +0300

    cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3333;
+const cors = require('cors');
+
+app.use(cors());
 
 app.use('/', express.static('public'));
 
