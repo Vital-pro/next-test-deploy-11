5916e69a430c2d737fbcbb03d23582456c33e1ae
Vital-pro <vitupikov@yandex.ru>
2024-06-27 22:37:20 +0300

    inputs1

--- public/client.js
+++ public/client.js
@@ -4,7 +4,7 @@ let lonInput = document.querySelector('#lon');
 let loadBtn = document.querySelector('#load');
 console.log(latInput.value);
 loadBtn.addEventListener('click', () => {
-  fetch(`/data?lat=37.26&lon=37.26`)
+  fetch(`https://next-test-deploy-11.vercel.app/data?lat=37.26&lon=37.26`)
     .then((res) => res.json())
     .then((resJson) => {
       // console.log(resJson);
